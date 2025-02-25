4d565756d0d109bf26b4c3a466015587e4cb948b
Jairosz <Jairosg03@gmail.com>
2025-02-25 22:48:11 +0100

    Setup inicial
    Setup inicial

---
+++ EcoinverGMAO_api/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EcoinverGMAO_api.data
+{
+    public class AppDbContext : DbContext
+    {
+        // Constructor opcional para inyección de dependencia
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Ejemplo de DbSet - Tus entidades irían aquí
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
---
+++ EcoinverGMAO_api/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using EcoinverGMAO_api.data;
+using System;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Agrega el DbContext a los servicios (ESTRUCTURA DE LA DB)
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("MySqlConnection"),
+        new MySqlServerVersion(new Version(8, 0, 32)) // Ajusta la versión de MySQL a la que uses
+    )
+);
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
