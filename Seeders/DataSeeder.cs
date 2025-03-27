efc8626481f8a04f44d515267bbf5508323c1c82
Ecoinver-Software <software@ecoinver.es>
2025-03-27 08:48:52 +0100

    Update DataSeeder.cs

--- Seeders/DataSeeder.cs
+++ Seeders/DataSeeder.cs
@@ -12,170 +12,168 @@ namespace EcoinverGMAO_api.Seeders
             var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
             var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<Role>>();
 
-            // Nombre del rol y datos del usuario administrador
-            string superAdminRoleName = "SuperAdmin";
-            string adminRoleName = "Admin";
-            string reporterRoleName = "Reportador";
-            string technicalRoleName = "Tecnico";
+            // Definir nombres de roles y usuarios
+            string developerRoleName = "desarrollador";
+            string adminRoleName = "admin";
+            string technicalRoleName = "tecnico de campo";
+            string comercialRoleName = "comercial";
+
             string adminUserName = "admin";
             string tecnicoUserName = "tecnico";
-            string reporterUserName = "reportador";
+            string comercialUserName = "comercial";
 
-            //Crear el rol de SuperAdministrador si no existe
-            if (!await roleManager.RoleExistsAsync(superAdminRoleName))
+            // Crear el rol de desarrollador si no existe
+            if (!await roleManager.RoleExistsAsync(developerRoleName))
             {
-                var Role = new Role
+                var role = new Role
                 {
-                    Name = superAdminRoleName,
-                    Description = "Rol de SuperAdministrador con permisos completos para desarrolladores",
+                    Name = developerRoleName,
+                    Description = "Rol de desarrollador con permisos completos para desarrolladores.",
                     Level = 1
                 };
 
-                var roleResult = await roleManager.CreateAsync(Role);
+                var roleResult = await roleManager.CreateAsync(role);
                 if (!roleResult.Succeeded)
                 {
-                    throw new Exception("Error al crear el rol de SuperAdministrador: " +
+                    throw new Exception("Error al crear el rol de desarrollador: " +
                         string.Join(", ", roleResult.Errors));
                 }
             }
 
-            //Crear el rol de administrador si no existe
+            // Crear el rol de admin si no existe
             if (!await roleManager.RoleExistsAsync(adminRoleName))
             {
-                var Role = new Role
+                var role = new Role
                 {
                     Name = adminRoleName,
-                    Description = "Rol de administrador con permisos completos",
+                    Description = "Rol de administrador con permisos completos.",
                     Level = 2
                 };
 
-                var roleResult = await roleManager.CreateAsync(Role);
+                var roleResult = await roleManager.CreateAsync(role);
                 if (!roleResult.Succeeded)
                 {
-                    throw new Exception("Error al crear el rol de administrador: " +
+                    throw new Exception("Error al crear el rol de admin: " +
                         string.Join(", ", roleResult.Errors));
                 }
             }
 
-            //Crear el rol de reportador si no existe
-            if (!await roleManager.RoleExistsAsync(reporterRoleName))
-            {
-                var Role = new Role
-                {
-                    Name = reporterRoleName,
-                    Description = "Rol de reportador con permisos limitados solo para reportar incidencias.",
-                    Level = 20
-                };
-
-                var roleResult = await roleManager.CreateAsync(Role);
-                if (!roleResult.Succeeded)
-                {
-                    throw new Exception("Error al crear el rol de reportador: " +
-                        string.Join(", ", roleResult.Errors));
-                }
-            }
-
-            //Crear el rol de tecnico si no existe
+            // Crear el rol de tecnico de campo si no existe
             if (!await roleManager.RoleExistsAsync(technicalRoleName))
             {
-                var Role = new Role
+                var role = new Role
                 {
                     Name = technicalRoleName,
-                    Description = "Rol de tecnico con permisos limitados para tecnicos.",
-                    Level = 20
+                    Description = "Rol de técnico de campo con permisos para tareas de campo.",
+                    Level = 3
                 };
 
-                var roleResult = await roleManager.CreateAsync(Role);
+                var roleResult = await roleManager.CreateAsync(role);
                 if (!roleResult.Succeeded)
                 {
-                    throw new Exception("Error al crear el rol de reportador: " +
+                    throw new Exception("Error al crear el rol de tecnico de campo: " +
                         string.Join(", ", roleResult.Errors));
                 }
             }
 
-            //  Crear el usuario administrador si no existe
+            // Crear el rol de comercial si no existe
+            if (!await roleManager.RoleExistsAsync(comercialRoleName))
+            {
+                var role = new Role
+                {
+                    Name = comercialRoleName,
+                    Description = "Rol de comercial con permisos limitados para tareas comerciales.",
+                    Level = 4
+                };
+
+                var roleResult = await roleManager.CreateAsync(role);
+                if (!roleResult.Succeeded)
+                {
+                    throw new Exception("Error al crear el rol de comercial: " +
+                        string.Join(", ", roleResult.Errors));
+                }
+            }
+
+            // Crear el usuario admin si no existe
             var adminUser = await userManager.FindByNameAsync(adminUserName);
             if (adminUser == null)
             {
                 adminUser = new User
                 {
                     UserName = adminUserName,
-                    Email = "admin@example.com",
+                    Email = "admin@ecoinver.es",
                     NombreCompleto = "Administrador del Sistema",
                     EmailConfirmed = true
                 };
 
-                // Crea el usuario con la contraseña "1234"
                 var userResult = await userManager.CreateAsync(adminUser, "1234");
                 if (!userResult.Succeeded)
                 {
-                    throw new Exception("Error al crear el usuario administrador: " +
+                    throw new Exception("Error al crear el usuario admin: " +
                         string.Join(", ", userResult.Errors));
                 }
 
-                // Asigna el rol de administrador al usuario creado
                 var addToRoleResult = await userManager.AddToRoleAsync(adminUser, adminRoleName);
                 if (!addToRoleResult.Succeeded)
                 {
-                    throw new Exception("Error al asignar el rol de administrador: " +
+                    throw new Exception("Error al asignar el rol de admin: " +
                         string.Join(", ", addToRoleResult.Errors));
                 }
             }
+
+            // Crear el usuario tecnico si no existe
             var tecnicoUser = await userManager.FindByNameAsync(tecnicoUserName);
             if (tecnicoUser == null)
             {
                 tecnicoUser = new User
                 {
                     UserName = tecnicoUserName,
-                    Email = "tecnico@example.com",
-                    NombreCompleto = "Tecnico",
+                    Email = "tecnico@ecoinver.es",
+                    NombreCompleto = "Técnico de Campo",
                     EmailConfirmed = true
                 };
 
-                // Crea el usuario con la contraseña "1234"
                 var userResult = await userManager.CreateAsync(tecnicoUser, "1234");
                 if (!userResult.Succeeded)
                 {
-                    throw new Exception("Error al crear el usuario tecnico: " +
+                    throw new Exception("Error al crear el usuario técnico: " +
                         string.Join(", ", userResult.Errors));
                 }
 
-                // Asigna el rol de administrador al usuario creado
                 var addToRoleResult = await userManager.AddToRoleAsync(tecnicoUser, technicalRoleName);
                 if (!addToRoleResult.Succeeded)
                 {
-                    throw new Exception("Error al asignar el rol de administrador: " +
+                    throw new Exception("Error al asignar el rol de tecnico de campo: " +
                         string.Join(", ", addToRoleResult.Errors));
                 }
             }
-            var reporterUser = await userManager.FindByNameAsync(reporterUserName);
-            if (reporterUser == null)
+
+            // Crear el usuario comercial si no existe
+            var comercialUser = await userManager.FindByNameAsync(comercialUserName);
+            if (comercialUser == null)
             {
-                reporterUser = new User
+                comercialUser = new User
                 {
-                    UserName = reporterUserName,
-                    Email = "reporter@example.com",
-                    NombreCompleto = "Reporter",
+                    UserName = comercialUserName,
+                    Email = "comercial@ecoinver.es",
+                    NombreCompleto = "Usuario Comercial",
                     EmailConfirmed = true
                 };
 
-                // Crea el usuario con la contraseña "1234"
-                var userResult = await userManager.CreateAsync(reporterUser, "1234");
+                var userResult = await userManager.CreateAsync(comercialUser, "1234");
                 if (!userResult.Succeeded)
                 {
-                    throw new Exception("Error al crear el usuario reporter: " +
+                    throw new Exception("Error al crear el usuario comercial: " +
                         string.Join(", ", userResult.Errors));
                 }
 
-                // Asigna el rol de reporter al usuario creado
-                var addToRoleResult = await userManager.AddToRoleAsync(reporterUser, reporterRoleName);
+                var addToRoleResult = await userManager.AddToRoleAsync(comercialUser, comercialRoleName);
                 if (!addToRoleResult.Succeeded)
                 {
-                    throw new Exception("Error al asignar el rol de reporter: " +
+                    throw new Exception("Error al asignar el rol de comercial: " +
                         string.Join(", ", addToRoleResult.Errors));
                 }
             }
-
         }
     }
 }
