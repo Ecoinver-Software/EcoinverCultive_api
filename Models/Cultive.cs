f16690e4202c5222bfc4834687bf0289ee872231
Ecoinver-Software <software@ecoinver.es>
2025-04-04 14:41:00 +0200

    ForeignKeyFix
    ForeignKeyFix

--- Models/Cultive.cs
+++ Models/Cultive.cs
@@ -1,33 +1,41 @@
-﻿namespace EcoinverGMAO_api.Models.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcoinverGMAO_api.Models.Entities
 {
     public class Cultive : BaseModel
     {
-        public int Id { get; set; } // Clave primaria autoincremental local
+        public int Id { get; set; } 
 
-        public int IdCultivo { get; set; } // ID original del ERP
+        public int IdCultivo { get; set; }
+
         public int? IdAgricultor { get; set; }
         public string NombreAgricultor { get; set; }
+
         public int? IdFinca { get; set; }
         public string NombreFinca { get; set; }
+
         public int? IdNave { get; set; }
         public string NombreNave { get; set; }
+
         public int? IdGenero { get; set; }
         public string NombreGenero { get; set; }
+
         public string NombreVariedad { get; set; }
+
         public double? Superficie { get; set; }
         public double? ProduccionEstimada { get; set; }
+
         public DateTime? FechaSiembra { get; set; }
         public DateTime? FechaFin { get; set; }
 
-        // Nuevos campos para latitud y longitud (almacenados como varchar(9))
+        // Campos de latitud y longitud como varchar(9) en base de datos
         public string Latitud { get; set; }
         public string Longitud { get; set; }
 
-        // Nuevo campo FK para el planning de cultivo
+        // FK para la tabla CultivePlanning
         public int? IdCultivePlanning { get; set; }
 
-        // Propiedad de navegación para la tabla CultivePlanning
-        // (opcional pero recomendable si utilizas Entity Framework)
+        [ForeignKey(nameof(IdCultivePlanning))]
         public virtual CultivePlanning CultivePlanning { get; set; }
     }
 }
