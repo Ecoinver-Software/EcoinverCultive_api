25fb3615831f57e664d0e9dee6098a92eca4996a
Melk236 <162185560+Melk236@users.noreply.github.com>
2025-05-08 13:58:58 +0200

    Endpoint para el ranking
    Endpoint para el ranking

---
+++ Controllers/CultiveDataRealController.cs
@@ -0,0 +1,27 @@
+﻿using EcoinverGMAO_api.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcoinverGMAO_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CultiveDataRealController : ControllerBase
+    {
+        private readonly AppDbContext _dbContext;
+
+        public CultiveDataRealController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: api/cultivedatareal
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CultiveDataReal>>> GetAll()
+        {
+            var data = await _dbContext.CultiveProductionReal.ToListAsync();
+            return Ok(data);
+        }
+}
+
+}
