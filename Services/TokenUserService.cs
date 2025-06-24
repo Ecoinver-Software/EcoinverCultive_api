18d3875e85a053b2bfeb1f95d149eb9b5debd88e
carmona891 <franciscofdezcarmona01@gmail.com>
2025-06-24 08:27:20 +0200

    Changes
    Changes

---
+++ Services/TokenUserService.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Claims;
+using EcoinverCultive_api.Models.Dto;
+
+
+namespace EcoinverCultive_api.Services
+{
+    public interface ITokenUserService
+    {
+        UserInfo GetUserFromToken(ClaimsPrincipal principal);
+    }
+
+    public class TokenUserService : ITokenUserService
+    {
+        public UserInfo GetUserFromToken(ClaimsPrincipal principal)
+        {
+            var username = principal.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name")?.Value;
+            var userId = principal.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier")?.Value;
+            var role = principal.FindFirst("http://schemas.microsoft.com/ws/2008/06/identity/claims/role")?.Value;
+            var roleLevel = principal.FindFirst("RoleLevel")?.Value;
+
+            return new UserInfo
+            {
+                Id = userId,
+                Username = username,
+                Role = role,
+                RoleLevel = int.Parse(roleLevel ?? "0")
+            };
+        }
+    }
+}
